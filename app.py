b3ae47f8a4751e4a3182acff5d77345670270ea2
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-05-23 10:29:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ X = tt2.iloc[:, :-1]
 y = tt2.iloc[:, -1] -1
 
 # train, test 데이터셋 나누기
-rs = st.number_input('머신러닝을 위한 무작위 숫자 입력', 1)
+#rs = st.number_input('Input a randome seed for machine leraning', 1)
+rs = 1030
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = rs)
 
 models = ['Logistic Regression', 'Support Vector Machine', 'Random Forest', 'XGBoost']  #'Decision Tree'
