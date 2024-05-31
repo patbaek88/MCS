fff07f5df3063f54db02d0a33f9fd4ad86943240
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-05-31 16:44:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ Excipient2_name = st.selectbox(
 Excipient2_content_f = 100 -API_content_f -Excipient1_content_f -Excipient3_content_f -Excipient4_content_f
 Excipient2_content = str(Excipient2_content_f)
 st.write("Filler content: "+Excipient2_content)
-if Excipient2_content <0:
+if float(Excipient2_content) <0:
     st.write("Error: Please adjust contents")
 
 st.write("")
