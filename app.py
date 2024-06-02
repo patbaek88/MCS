fed2393bec0408f2d10809a7f656f4b597db8bd4
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-06-02 13:23:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,11 +87,15 @@ y_1 = tt2_1.iloc[:, -1]
 # train, test 데이터셋 나누기
 X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X_1, y_1, test_size=0.2)
 
-rf_1 = RandomForestClassifier()
-rf_1.fit(X_train_1, y_train_1)
-rf_pred_1 = rf_1.predict(X_test_1)
-rf_acc_1 = accuracy_score(y_test_1, rf_pred_1)
+#rf_1 = RandomForestClassifier()
+#rf_1.fit(X_train_1, y_train_1)
+#rf_pred_1 = rf_1.predict(X_test_1)
+#rf_acc_1 = accuracy_score(y_test_1, rf_pred_1)
 
+lgbm_1 = LGBMClassifier(n_estimators=30)
+lgbm_1.fit(X_train_1, y_train_1)
+lgbm_pred_1 = lgbm_1.predict(X_test_1)
+lgbm_acc_1 = accuracy_score(y_test_1, lgbm_pred_1)
 
 
 st.subheader(" ")
@@ -102,7 +106,7 @@ API_list = API_df.index.to_list()
 API_name = st.selectbox('API', API_list, index = 34)
 
 API_pc = principalDf_1.loc[principalDf_1.index == API_name].iloc[:, :].values
-API_class = rf_1.predict(API_pc)
+API_class = lgbm_1.predict(API_pc)
 st.write("API only : Class "+API_class[0])
 strength = st.number_input('API content (mg)', 1, 1000, value = 100)
 tablet_wt = st.number_input('Tablet weight (mg)', strength, 1000, value = 200)
