02b149abeb8733f60543d8f9f687963a6a3c3977
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-06-06 00:38:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -566,7 +566,7 @@ if st.button("Predict"):
             f1score = f1_score(y_test, y_pred, average='macro')
             f1scores.append(f1score)
             if y_pred_proba is not None:
-                rocauc = roc_auc_score(y_test, y_pred_proba, multi_class='ovr' if len(set(y_test))>2 else 'raise')
+                rocauc = roc_auc_score(y_test, y_pred_proba, multi_class='ovr' if len(np.unique(y_test))>2 else 'raise')
             rocaucs.append(rocauc)
             #logloss = log_loss(y_test, y_pred)
             #loglosses.append(logloss)
