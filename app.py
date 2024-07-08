1f67280a2584c01218f50c0c8a9a203a11291843
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-07-08 17:28:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -532,7 +532,7 @@ if st.button("Predict"):
         'Random Forest': RandomForestClassifier(),
         'K-Nearest Neighbors': KNeighborsClassifier(),
         'LightGBM': LGBMClassifier(n_estimators=30),
-        'SGD': SGDClassifier(class_weight = 'balanced')
+        'SGD': SGDClassifier(loss='log', class_weight = 'balanced')
     }
 
     results = []
