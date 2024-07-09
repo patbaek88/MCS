8070e10a2e17efd0cd0b2e1e82c786665e7f8be7
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-07-09 09:06:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, label_binarize
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.feature_selection import SelectKBest
 from sklearn.linear_model import LogisticRegression, SGDClassifier
